8f8b1e285cd5c913b907ed4ed1e5f5559dc91e80
SpeedBird789 <asifmahmoud787@gmail.com>
2022-04-17 00:11:38 +0100

    services section structure done

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+           <p><small> All Rights Reserved ©</small></p>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/firebase.init.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAlP4YMdf6xtjcmrBnfPSUwpLw600HNJDY",
+  authDomain: "independent-service-prov-374f1.firebaseapp.com",
+  projectId: "independent-service-prov-374f1",
+  storageBucket: "independent-service-prov-374f1.appspot.com",
+  messagingSenderId: "769821978245",
+  appId: "1:769821978245:web:8a7c9cc76edcd2185a03ec"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export default auth;
