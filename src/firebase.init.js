675e72625c9d27af42cc2007b0645926daa3462d
SpeedBird789 <asifmahmoud787@gmail.com>
2022-04-17 16:25:55 +0100

    environment variable done

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -4,14 +4,15 @@ import {getAuth} from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAlP4YMdf6xtjcmrBnfPSUwpLw600HNJDY",
-  authDomain: "independent-service-prov-374f1.firebaseapp.com",
-  projectId: "independent-service-prov-374f1",
-  storageBucket: "independent-service-prov-374f1.appspot.com",
-  messagingSenderId: "769821978245",
-  appId: "1:769821978245:web:8a7c9cc76edcd2185a03ec"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain, 
+  projectId:process.env.REACT_APP_projectId, 
+  storageBucket:process.env.REACT_APP_storageBucket, 
+  messagingSenderId:process.env.REACT_APP_messagingSenderId, 
+  appId:process.env.REACT_APP_appId, 
 };
 
 // Initialize Firebase
