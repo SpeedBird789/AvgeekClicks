48e29c11e270b88f4748a9a9ba40f3d9b0dff06b
SpeedBird789 <asifmahmoud787@gmail.com>
2022-04-17 01:10:50 +0100

    example work section structure done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Services from '../Services/Services';
+import Works from '../Works/Works';
 
 const Home = () => {
     return (
         <div>
             <Services></Services>
+            <Works></Works>
         </div>
     );
 };
---
+++ src/Pages/Home/Works/Works.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import biman from '../../../images//example work/biman.png';
+import sg from '../../../images//example work/sg.png';
+import gulf from '../../../images//example work/gulf.png';
+import Work from '../Work/Work';
+
+const works = [
+    {id: 1, name: 'Changi Diaries', img: sg},
+    {id: 2, name: 'Sweet Home HSIA', img: biman},
+    {id: 3, name: 'Arabian Luxury', img: gulf},
+]
+
+const Works = () => {
+    return (
+        <div className='container'>
+            <h2 className='text-primary text-center'>Example Works</h2>
+            <div className='row'>
+                {
+                    works.map(work => <Work key={work.id} work={work}></Work>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Works;
