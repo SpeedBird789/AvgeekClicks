1975e4f908e906437803531593ec4be6fae3620b
SpeedBird789 <asifmahmoud787@gmail.com>
2022-04-18 14:28:20 +0100

    about me done

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h2>This is about me</h2>
+        <div className='container'>
+            <h2 className='text-center mt-5'>Hello there!</h2>
+            
+
+            <h5 className='mt-4'>I'm <span className='text-danger'>Asif</span>, an engineering student trying to ride my luck somewhere on this planet. In this diverse and fast-moving world, where any skill is attainable and could be the one you do for the rest of your life, just giving a shot to learn a bit of Web Developemnt. Started learning it on the 1st of Januray this year and the process is still ongoing, little by little coming to and end of the learning path. Now the goal is to finish it with satisfaction. Trying to stay on track and finish it on a high, let's see where it leads me to. Thanks for coming to my TEDTalk :)  </h5>
         </div>
     );
 };
